2191b2de67452f4efe5fd4e1e88e0831c166fe54
Shunya Sago <shun.s0029@gmail.com>
2021-08-12 06:10:36 +0000

    [Add] header footer完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :books
   root to: 'homes#top'
+  get "about" => 'homes#about'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
