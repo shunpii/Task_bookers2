384f4c2b67c8a58b27941afa03c75921d8350874
Shunya Sago <shun.s0029@gmail.com>
2021-08-14 02:46:22 +0000

    [Add] book-index左側作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
                                     sessions: 'users/sessions' }
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
