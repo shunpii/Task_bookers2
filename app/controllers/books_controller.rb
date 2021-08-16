b31b5fbe18909614f3e2cfded0c859630588804f
Shunya Sago <shun.s0029@gmail.com>
2021-08-16 16:03:55 +0000

    [add] book index username表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path()
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
                                     sessions: 'users/sessions' }
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
 
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
