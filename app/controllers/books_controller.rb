215145efc09811e6a9d552e48cbafc97a8b48830
Shunya Sago <shun.s0029@gmail.com>
2021-08-22 00:51:49 +0000

    [add] フラッシュメッセージ、ユーザログイン以外完

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    end
   end
 
   def index
@@ -20,8 +21,9 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    end
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to books_path(@user.id)
+    if @user.update(user_params)
+      redirect_to books_path(@user.id), notice: "You have updated user successfully."
+    end
   end
 
   private
